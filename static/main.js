88913099a5278f33c1a2cc7d46b4d1a7afc0c999
lisanmorrison <lisamorrison93@live.co.uk>
2014-05-30 15:47:01 -0700

    I have drawn preliminary racks and the temperature sensors

--- static/main.js
+++ static/main.js
@@ -6,35 +6,201 @@ function renderRacks(){
 
 	var width = 0.8*window.innerWidth,
 		height = 0.8*window.innerHeight,
-		grid = Math.min(width/65, height/65);
+		grid = Math.min(width/67, height/64);
 
 	rackImage = {};
 	cells = {};
+	sensors = {}
 
-    ////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
     //Kinetic.js setup
-    ////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
 	rackImage.stage = new Kinetic.Stage({
             container: 'shackStatus',
             width: width,
             height: height
      });
 
+	///////////////////////////////////////////////////////////////////////////
 	//make main layer
+    ///////////////////////////////////////////////////////////////////////////
 	rackImage.mainLayer = new Kinetic.Layer();
 	rackImage.stage.add(rackImage.mainLayer);
 
-	cells.rect = new Kinetic.Rect({
-		x: 10*grid,
-		y: 34*grid,
-		width: 12*grid,
-		height: 6*grid,
-		fill: 'green',
+    ///////////////////////////////////////////////////////////////////////////
+	//This sets up the racks
+    ///////////////////////////////////////////////////////////////////////////
+	cells.rack1 = new Kinetic.Rect({
+		x: 33*grid,
+		y: 4*grid,
+		width: 20*grid,
+		height: 56*grid,
+		fill: 'white',
 		stroke: 'black',
-		strokeWidth: 4
+		strokeWidth: 2
 	});
 
-	rackImage.mainLayer.add(cells.rect);
+	cells.rack2 = new Kinetic.Rect({
+		x: 53*grid,
+		y: 4*grid,
+		width: 20*grid,
+		height: 56*grid,
+		fill: 'white',
+		stroke: 'black',
+		strokeWidth: 2
+	});
+
+
+	cells.rack3 = new Kinetic.Rect({
+		x: 73*grid,
+		y: 4*grid,
+		width: 20*grid,
+		height: 56*grid,
+		fill: 'white',
+		stroke: 'black',
+		strokeWidth: 2
+	});
+
+
+	cells.rack4 = new Kinetic.Rect({
+		x: 93*grid,
+		y: 4*grid,
+		width: 20*grid,
+		height: 56*grid,
+		fill: 'white',
+		stroke: 'black',
+		strokeWidth: 2
+	});
+
+
+	cells.rack5 = new Kinetic.Rect({
+		x: 113*grid,
+		y: 4*grid,
+		width: 20*grid,
+		height: 56*grid,
+		fill: 'white',
+		stroke: 'black',
+		strokeWidth: 2
+	});
+
+    ///////////////////////////////////////////////////////////////////////////
+	//Adding temperature sensors
+    ///////////////////////////////////////////////////////////////////////////
+
+	cells.sensor1 = new Kinetic.Rect({
+		x: 35*grid,
+		y: 2*grid,
+		width: 16*grid,
+		height: 2*grid,
+		fill: 'gray',
+		stroke: 'black',
+		strokeWidth: 2
+	})
+
+		cells.sensor2 = new Kinetic.Rect({
+		x: 55*grid,
+		y: 2*grid,
+		width: 16*grid,
+		height: 2*grid,
+		fill: 'gray',
+		stroke: 'black',
+		strokeWidth: 2
+	})
+
+	cells.sensor3 = new Kinetic.Rect({
+		x: 75*grid,
+		y: 2*grid,
+		width: 16*grid,
+		height: 2*grid,
+		fill: 'gray',
+		stroke: 'black',
+		strokeWidth: 2
+	})
+
+	cells.sensor4 = new Kinetic.Rect({
+		x: 95*grid,
+		y: 2*grid,
+		width: 16*grid,
+		height: 2*grid,
+		fill: 'gray',
+		stroke: 'black',
+		strokeWidth: 2
+	})
+
+		cells.sensor5 = new Kinetic.Rect({
+		x: 115*grid,
+		y: 2*grid,
+		width: 16*grid,
+		height: 2*grid,
+		fill: 'gray',
+		stroke: 'black',
+		strokeWidth: 2
+	})
+
+		cells.sensor6 = new Kinetic.Rect({
+		x: 35*grid,
+		y: 60*grid,
+		width: 16*grid,
+		height: 2*grid,
+		fill: 'gray',
+		stroke: 'black',
+		strokeWidth: 2
+	})
+
+		cells.sensor7 = new Kinetic.Rect({
+		x: 55*grid,
+		y: 60*grid,
+		width: 16*grid,
+		height: 2*grid,
+		fill: 'gray',
+		stroke: 'black',
+		strokeWidth: 2
+	})
+		cells.sensor8 = new Kinetic.Rect({
+		x: 75*grid,
+		y: 60*grid,
+		width: 16*grid,
+		height: 2*grid,
+		fill: 'gray',
+		stroke: 'black',
+		strokeWidth: 2
+	})
+		cells.sensor9 = new Kinetic.Rect({
+		x: 95*grid,
+		y: 60*grid,
+		width: 16*grid,
+		height: 2*grid,
+		fill: 'gray',
+		stroke: 'black',
+		strokeWidth: 2
+	})
+
+		cells.sensor10 = new Kinetic.Rect({
+		x: 115*grid,
+		y: 60*grid,
+		width: 16*grid,
+		height: 2*grid,
+		fill: 'gray',
+		stroke: 'black',
+		strokeWidth: 2
+	})
+
+	rackImage.mainLayer.add(cells.rack1);
+	rackImage.mainLayer.add(cells.rack2);
+	rackImage.mainLayer.add(cells.rack3);
+	rackImage.mainLayer.add(cells.rack4);
+	rackImage.mainLayer.add(cells.rack5);
+
+	rackImage.mainLayer.add(cells.sensor1);
+	rackImage.mainLayer.add(cells.sensor2);
+	rackImage.mainLayer.add(cells.sensor3);
+	rackImage.mainLayer.add(cells.sensor4);
+	rackImage.mainLayer.add(cells.sensor5);
+	rackImage.mainLayer.add(cells.sensor6);
+	rackImage.mainLayer.add(cells.sensor7);
+	rackImage.mainLayer.add(cells.sensor8);
+	rackImage.mainLayer.add(cells.sensor9);
+	rackImage.mainLayer.add(cells.sensor10);
 
 	rackImage.mainLayer.draw();
 }
